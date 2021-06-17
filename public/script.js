4cd86597400ac56b23de6cf238c3f0828d37569d
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-06-18 04:08:04 +0530

    First Commit

---
+++ public/script.js
@@ -0,0 +1,83 @@
+let ground = document.querySelector(".playground .ground")
+let allCells = []
+let snakeSpeed = 0.3;
+let snakeLength = 1;
+
+for(let i = 0; i < 20; i++) {
+    allCells[i] = []
+    for (let j = 0; j < 20; j++) {
+        let cell = document.createElement("div")
+        allCells[i].push(cell)
+        cell.className = "cell"
+        ground.append(cell)
+    }
+}
+
+const setFood = () => {
+    let i = Math.floor(Math.random() * allCells.length);
+    let j = Math.floor(Math.random() * allCells[i].length);
+    allCells[i][j].classList.add("food");
+}
+
+let currentDirection = [1, 0]
+let currentPosition = [0, 0]
+let intervalId = 0
+
+const gameOver = () => {
+    console.log("Game Over")
+}
+
+const atSnakeAte = () => {
+    console.log("Snake ate")
+    snakeLength++;
+    setFood();
+}
+
+const moveSnake = () => {
+    document.body.style.setProperty("--snake-length", snakeLength + "")
+    document.body.style.setProperty("--snake-speed", snakeSpeed + "s")
+
+    if(allCells[currentPosition[0]][currentPosition[1]].classList.contains("snake")){
+        let i = currentPosition[0];
+        let j = currentPosition[1];
+        allCells[i][j].classList.remove("snake")
+        setTimeout(() => {
+            allCells[i][j].classList.remove("tail")
+        }, (snakeLength-1) * snakeSpeed * 1000)
+        allCells[i][j].classList.add("tail")
+    }
+
+    currentPosition = currentPosition.map((p, i) => {
+        return p + currentDirection[1-i]
+    })
+
+    if(currentPosition[0] < 0 || currentPosition[0] > allCells.length-1
+        || currentPosition[1] < 0 || currentPosition[1] > allCells.length-1){
+        gameOver()
+        clearInterval(intervalId)
+        return
+    }
+
+    if(allCells[currentPosition[0]][currentPosition[1]].classList.contains("food")){
+        allCells[currentPosition[0]][currentPosition[1]].classList.remove("food")
+        atSnakeAte()
+    }
+    allCells[currentPosition[0]][currentPosition[1]].classList.add("snake")
+}
+
+allCells[0][0].classList.add("snake")
+intervalId = setInterval(moveSnake, snakeSpeed * 1000)
+
+const keyEventMap = {
+    ArrowUp: () => currentDirection = [0, -1],
+    ArrowDown: () => currentDirection = [0, 1],
+    ArrowRight: () => currentDirection = [1, 0],
+    ArrowLeft: () => currentDirection = [-1, 0]
+}
+
+document.body.addEventListener("keydown", (event) => {
+    if(keyEventMap.hasOwnProperty(event.key))
+        keyEventMap[event.key]()
+})
+
+setFood()
