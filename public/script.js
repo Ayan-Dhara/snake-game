da278339c338dc20be67aae8b2018aafde2337eb
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-06-19 18:53:01 +0530

    Improved game 🐍
    1. Add self collition
    2. Removed wall collition
    3. changed snake presentation
    4. Added Score Alert

--- public/script.js
+++ public/script.js
@@ -16,15 +16,23 @@ for(let i = 0; i < 20; i++) {
 const setFood = () => {
     let i = Math.floor(Math.random() * allCells.length);
     let j = Math.floor(Math.random() * allCells[i].length);
-    allCells[i][j].classList.add("food");
+    if(allCells[i][j].classList.contains("tail"))
+        setFood()
+    else if(allCells[i][j].classList.contains("snake"))
+        setFood()
+    else
+        allCells[i][j].classList.add("food");
 }
 
-let currentDirection = [1, 0]
+let currentDirection = [0, 0]
 let currentPosition = [0, 0]
+let lastMove = [0, 0]
+let currentLayout = 0
 let intervalId = 0
 
 const gameOver = () => {
-    console.log("Game Over")
+    clearInterval(intervalId)
+    alert("Game Over. Your Score is " + snakeLength)
 }
 
 const atSnakeAte = () => {
@@ -34,8 +42,14 @@ const atSnakeAte = () => {
 }
 
 const moveSnake = () => {
+    if(currentDirection[0] === 0 && currentDirection[1] === 0)
+        return;
+    else
+        console.log("moving")
+
     document.body.style.setProperty("--snake-length", snakeLength + "")
     document.body.style.setProperty("--snake-speed", snakeSpeed + "s")
+
 
     if(allCells[currentPosition[0]][currentPosition[1]].classList.contains("snake")){
         let i = currentPosition[0];
@@ -43,18 +57,36 @@ const moveSnake = () => {
         allCells[i][j].classList.remove("snake")
         setTimeout(() => {
             allCells[i][j].classList.remove("tail")
+            allCells[i][j].classList.remove("horizontal")
+            allCells[i][j].classList.remove("vertical")
         }, (snakeLength-1) * snakeSpeed * 1000)
         allCells[i][j].classList.add("tail")
+        allCells[i][j].classList.add(currentLayout?"vertical":"horizontal")
     }
 
     currentPosition = currentPosition.map((p, i) => {
         return p + currentDirection[1-i]
     })
 
-    if(currentPosition[0] < 0 || currentPosition[0] > allCells.length-1
-        || currentPosition[1] < 0 || currentPosition[1] > allCells.length-1){
+    // if(currentPosition[0] < 0 || currentPosition[0] > allCells.length-1
+    //     || currentPosition[1] < 0 || currentPosition[1] > allCells.length-1){
+    //     gameOver()
+    //     // clearInterval(intervalId)
+    //     return
+    // }
+
+    if(currentPosition[0] < 0)
+        currentPosition[0] = allCells.length - 1
+    if(currentPosition[0] > allCells.length-1)
+        currentPosition[0] = 0
+    if(currentPosition[1] < 0)
+        currentPosition[1] = allCells.length-1
+    if(currentPosition[1] > allCells.length-1)
+        currentPosition[1] = 0
+
+
+    if(allCells[currentPosition[0]][currentPosition[1]]?.classList?.contains("tail")){
         gameOver()
-        clearInterval(intervalId)
         return
     }
 
@@ -69,10 +101,22 @@ allCells[0][0].classList.add("snake")
 intervalId = setInterval(moveSnake, snakeSpeed * 1000)
 
 const keyEventMap = {
-    ArrowUp: () => currentDirection = [0, -1],
-    ArrowDown: () => currentDirection = [0, 1],
-    ArrowRight: () => currentDirection = [1, 0],
-    ArrowLeft: () => currentDirection = [-1, 0]
+    ArrowUp: () => {
+        currentDirection = [0, -1]
+        currentLayout = 1
+    },
+    ArrowDown: () => {
+        currentDirection = [0, 1]
+        currentLayout = 1
+    },
+    ArrowRight: () => {
+        currentDirection = [1, 0]
+        currentLayout = 0
+    },
+    ArrowLeft: () => {
+        currentDirection = [-1, 0]
+        currentLayout = 0
+    }
 }
 
 document.body.addEventListener("keydown", (event) => {
